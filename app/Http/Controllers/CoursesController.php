83ed372dca8587400d3d2c24ba9d423658591a6f
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2023-02-11 01:52:25 +0100

    w

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -9,16 +9,22 @@ use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
+
+public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $data = Courses::all();
         if($request->expectsJson()) {
-            return response()->json($data);
+            return response()->json(['message' => __("controller.success.get"), 'data' => $data]);
         }
         return view('courses.index', compact('data'));
     }
@@ -28,34 +34,12 @@ class CoursesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required',
-            'image' => 'required',
-            'category' => 'required',
-            'level' => 'required',
-            'duration' => 'required',
-            'start_date' => 'required',
-            'end_date' => 'required',
-            'status' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            if($request->expectsJson()) {
-                return response()->json($validator->errors());
-            }
-            return redirect()->back()->withErrors($validator->errors());
-        }
-        $validated = $validator->validated();
-
-        $data = Courses::create($validated);
         if($request->expectsJson()) {
-            return response()->json($data);
+            return response()->json(['message' => __("controller.error.not_found")], 404);
         }
-        return redirect()->back()->with('success', 'Course created successfully.');
+        return view('courses.create');
     }
 
     /**
@@ -66,7 +50,50 @@ class CoursesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        /**
+         * create table courses
+        (
+        id          bigint unsigned auto_increment
+        primary key,
+        name        varchar(255)    not null,
+        description varchar(255)    not null,
+        image       varchar(255)    not null,
+        author_id   bigint unsigned not null,
+        created_at  timestamp       null,
+        updated_at  timestamp       null,
+        constraint courses_author_id_foreign
+        foreign key (author_id) references users (id)
+        )
+         */
+        $user = Auth::user();
+        //see if (teacher and approved) or admin
+        if(!($user->is_teacher && $user->is_approved) && !$user->is_admin) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => __("controller.error.unauthorized")], 401);
+            }
+            return redirect()->back()->with('error', __("controller.error.unauthorized"));
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+        ]);
+
+
+        if ($validator->fails()) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => __("controller.error.validation"), 'errors' => $validator->errors()], 400);
+            }
+            return redirect()->back()->withErrors($validator->errors());
+        }
+        $validated = $validator->validated();
+        $validated['author_id'] = $user->id;
+        $data = Courses::create($validated);
+        if($request->expectsJson()) {
+            return response()->json(['message' => __("controller.success.created"), 'data' => $data], 201);
+        }
+        return redirect()->back()->with('success', __("controller.success.created"));
     }
 
     /**
@@ -75,9 +102,12 @@ class CoursesController extends Controller
      * @param  \App\Models\Courses  $courses
      * @return \Illuminate\Http\Response
      */
-    public function show(CoursesController $courses)
+    public function show(Request $request, CoursesController $courses)
     {
-        //
+        if($request->expectsJson()) {
+            return response()->json(['message' => __("controller.success.get"), 'data' => $data]);
+        }
+        return view("courses.show", compact('data'));
     }
 
     /**
@@ -86,9 +116,12 @@ class CoursesController extends Controller
      * @param  \App\Models\Courses  $courses
      * @return \Illuminate\Http\Response
      */
-    public function edit(CoursesController $courses)
+    public function edit(CoursesController $courses, Request $request)
     {
-        //
+        if($request->expectsJson()) {
+            return response()->json(['message' => __("controller.error.not_found")], 404);
+        }
+        return view('courses.edit', compact('data'));
     }
 
     /**
@@ -100,7 +133,34 @@ class CoursesController extends Controller
      */
     public function update(Request $request, CoursesController $courses)
     {
-        //
+        $user = Auth::user();
+        //see if (teacher and approved) or admin
+        if(!($user->is_teacher && $user->is_approved && $user->id == $courses->author_id) && !$user->is_admin) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => __("controller.error.unauthorized")], 401);
+            }
+            return redirect()->back()->with('error', __("controller.error.unauthorized"));
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => __("controller.error.validation"), 'errors' => $validator->errors()], 400);
+            }
+            return redirect()->back()->withErrors($validator->errors());
+        }
+
+        $validated = $validator->validated();
+        $data = $courses->update($validated);
+        if($request->expectsJson()) {
+            return response()->json(['message' => __("controller.success.updated"), 'data' => $data]);
+        }
+        return redirect()->back()->with('success', __("controller.success.updated"));
     }
 
     /**
@@ -109,8 +169,20 @@ class CoursesController extends Controller
      * @param  \App\Models\Courses  $courses
      * @return \Illuminate\Http\Response
      */
-    public function destroy(CoursesController $courses)
+    public function destroy(Request $request, CoursesController $courses)
     {
-        //
+        $user = Auth::user();
+        //see if (teacher and approved) or admin
+        if(!($user->is_teacher && $user->is_approved && $user->id == $courses->author_id) && !$user->is_admin) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => __("controller.error.unauthorized")], 401);
+            }
+            return redirect()->back()->with('error', __("controller.error.unauthorized"));
+        }
+        $result = $courses->delete();
+        if($request->expectsJson()) {
+            return response()->json(['message' => __("controller.success.deleted"), 'data' => $result]);
+        }
+        return redirect()->back()->with('success', __("controller.success.deleted"));
     }
 }
---
+++ lang/en/controller.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Controller Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'success' => array(
+        'create' => 'Created successfully.',
+        'update' => 'Updated successfully.',
+        'delete' => 'Deleted successfully.',
+        'get' => 'Retrieved successfully.',
+    ),
+    'error' => array(
+        'create' => 'Failed to create.',
+        'update' => 'Failed to update.',
+        'delete' => 'Failed to delete.',
+        'get' => 'Failed to retrieve.',
+        'unauthorized' => 'Unauthorized.',
+        'not_found' => 'Not found.',
+        'validation' => 'Validation failed.'
+    ),
+
+];
